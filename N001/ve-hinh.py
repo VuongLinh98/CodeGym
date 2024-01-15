a340b12c1dcd5f48401b96ad0d85e1b53c3d7913
Linh Vương <linhvuong@Linhs-MacBook-Pro.local>
2024-01-15 20:33:13 +0700

    tao file A va B

---
+++ N001/ve-hinh.py
@@ -0,0 +1,20 @@
+# Bước 1: lấy công cụ vẽ
+import turtle
+# bước 2: lấy viết
+t=turtle.Turtle()
+#bước 3: 
+# Tiến tới 100 đơn vị
+t.forward(100)
+# quay phải 90 độ
+t.right(90)
+# tiến tới 100 đơn vị
+t.forward(100)
+# rẽ phải
+t.right(90)
+#tiến tới 100 đơn vị
+t.forward(100)
+#quay phải 90 độ
+t.right(90)
+# tiến tới 100 đơn vị
+t.forward(100)
+turtle.done()
