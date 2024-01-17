5942837706731cac11ec43a72577ab018e313cbb
Vuong Linh <vghuyenlinh211@gmail.com>
2024-01-17 21:28:58 +0700

    03-bien toan tu va kieu du lieu

---
+++ C/bien.py
@@ -0,0 +1,5 @@
+ten_nhom = "C0124L1"
+so_thanh_vien = 20
+da_co_gia_dinh = False
+tong_can_nang = 65.5 
+name = "I'm Lin" 
---
+++ C/sum.py
@@ -0,0 +1,9 @@
+# INPUT A,B
+A = 5
+B = 7
+
+#XU Ly
+C = A + B 
+
+#XUAT
+print( C )
